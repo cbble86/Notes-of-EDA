010c531eb8740bf7a0809f162aca875ebff5ff69
cbble86 <cbble204@gmail.com>
2018-01-31 22:43:50 +0800

    Create PL_3_8_PIN_SET.v

---
+++ decoder38/PL_3_8_PIN_SET.v
@@ -0,0 +1,33 @@
+
+set_property PACKAGE_PIN T22 [get_ports {LED[0]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {LED[0]}]
+
+set_property PACKAGE_PIN T21 [get_ports {LED[1]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {LED[1]}]
+
+set_property PACKAGE_PIN U22 [get_ports {LED[2]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {LED[2]}]
+
+set_property PACKAGE_PIN U21 [get_ports {LED[3]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {LED[3]}]
+
+set_property PACKAGE_PIN V22 [get_ports {LED[4]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {LED[4]}]
+
+set_property IOSTANDARD LVCMOS33 [get_ports {LED[5]}]
+set_property PACKAGE_PIN W22 [get_ports {LED[5]}]
+
+set_property PACKAGE_PIN U19 [get_ports {LED[6]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {LED[6]}]
+
+set_property IOSTANDARD LVCMOS33 [get_ports {LED[7]}]
+set_property PACKAGE_PIN U14 [get_ports {LED[7]}]
+
+set_property IOSTANDARD LVCMOS33 [get_ports {SW0}]
+set_property PACKAGE_PIN F22 [get_ports {SW0}]
+
+set_property IOSTANDARD LVCMOS33 [get_ports {SW1}]
+set_property PACKAGE_PIN G22 [get_ports {SW1}]
+
+set_property IOSTANDARD LVCMOS33 [get_ports {SW2}]
+set_property PACKAGE_PIN H22 [get_ports {SW2}]
